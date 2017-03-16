0cfa4b6d5c1a9a80f24a58b9f8425aeff0901118
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-03-15 23:59:33 -0400

    x

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='pygitutils',
       description ='cross-platform git utilities for managing a large number of git repositories quickly',
       author = 'Michael Hirsch, Ph.D.',
       version = '1.0',
-      url = 'https://github.com/scienceopen/pygitutils',
+      url = 'https://github.com/scivision/pygitutils',
       classifiers=[
       'Development Status :: 5 - Production/Stable',
       'License :: OSI Approved :: MIT License',
