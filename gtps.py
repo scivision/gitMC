ee775ceebe846f112d75d018943d5dbf775bdb9b
irs4 <scivision@users.noreply.github.com>
2017-10-23 14:38:03 -0400

    cleanup

--- gtps.py
+++ gtps.py
@@ -27,13 +27,13 @@ def detectchange(d,verbose=False):
     dpath = None # in case error
     c= ['git','status','--porcelain'] # uncommitted or changed files
     try:
-        ret = S.check_output(c, cwd=d) #stderr=S.DEVNULL,
+        ret = subprocess.check_output(c, cwd=d) 
         if ret:
             dpath = d
             if verbose:
                 print(Back.MAGENTA + str(d))
                 print(Back.BLACK + ret.decode('utf8'))
-    except S.CalledProcessError as e:
+    except subprocess.CalledProcessError as e:
         print('Error in',d,e.output, file=stderr)
 
     return dpath
--- tests/test.py
+++ tests/test.py
@@ -4,5 +4,4 @@ import subprocess
 
 rdir = Path(__file__).parents[1]
 
-print(type(rdir))
 subprocess.check_call('./gtps.py ..'.split(), cwd=rdir)
