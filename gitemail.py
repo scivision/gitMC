859ec4824e2e558b49368942cde303b71d854021
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-10-25 00:04:01 -0400

    join

--- gitemail.py
+++ gitemail.py
@@ -16,11 +16,20 @@ gitemail.py -d ~/code -r
 """
 import sys
 from pathlib import Path
+import subprocess
 from pygitutils import gitemail
-#
 sys.tracebacklimit=1
 #
+cwd = Path(__file__).parent
 github = '@users.noreply.github.com'
+
+def amend(path:Path, emails:list, user:str):
+    assert isinstance(user,str)
+
+    for email in emails:
+        if email != user + github:
+            cmd = [str(cwd/'amender.sh'), email, user]
+            subprocess.check_call(cmd, cwd=path)
 
 
 if __name__ == '__main__':
@@ -33,6 +42,7 @@ if __name__ == '__main__':
     p.add_argument('-d','--dir',help='path to Git repo', nargs='?',default='.')
     p.add_argument('-r',help='recurse',action='store_true')
     p.add_argument('-e','--exclude',help='users to ignore (keep)',nargs='+')
+    p.add_argument('-a','--amend',help='change all non-exclused commits to username',action='store_true')
     p = p.parse_args()
 
     path = Path(p.dir).expanduser()
@@ -41,6 +51,8 @@ if __name__ == '__main__':
 
     for d in dlist:
         emails = gitemail(d, p.user, p.exclude)
+        if p.user and p.amend:
+            amend(d, emails, p.user)
 
 
 
