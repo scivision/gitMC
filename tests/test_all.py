ceb06f8a8597a037e6d3fa6087eef436e7eabe20
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-03-16 05:29:46 -0400

    windows accomodation in test

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 from pathlib import Path
-import subprocess
+import subprocess,os
 
 rdir = Path(__file__).parents[1]
 
+cmd = 'python ' if os.name == 'nt' else ''
+    
+
 def test_gitpush():
-    subprocess.check_call('gtps.py ..'.split(), cwd=rdir)
+    subprocess.check_call((cmd+'gtps.py ..').split(), cwd=rdir)
 
 def test_gitbranch():
-    subprocess.check_call('gitbranch.py ..'.split(), cwd=rdir)
+    subprocess.check_call((cmd+'gitbranch.py ..').split(), cwd=rdir)
 
 def test_actonchanged():
-    subprocess.check_call('ActOnChanged.py .'.split(), cwd=rdir)
+    subprocess.check_call((cmd+'ActOnChanged.py .').split(), cwd=rdir)
