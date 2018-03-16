9d0dbc13d8542bf8b2874b1574517b5a2a45561d
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-03-16 05:24:05 -0400

    test path

--- tests/test_all.py
+++ tests/test_all.py
@@ -5,10 +5,10 @@ import subprocess
 rdir = Path(__file__).parents[1]
 
 def test_gitpush():
-    subprocess.check_call('./gtps.py ..'.split(), cwd=rdir)
+    subprocess.check_call('gtps.py ..'.split(), cwd=rdir)
 
 def test_gitbranch():
-    subprocess.check_call('./gitbranch.py ..'.split(), cwd=rdir)
+    subprocess.check_call('gitbranch.py ..'.split(), cwd=rdir)
 
 def test_actonchanged():
-    subprocess.check_call('./ActOnChanged.py .'.split(), cwd=rdir)
+    subprocess.check_call('ActOnChanged.py .'.split(), cwd=rdir)
