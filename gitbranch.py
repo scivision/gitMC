cfa28e7da4da7847dbd4ac13ee330d02d22b24d1
scivision <scivision@users.noreply.github.com>
2018-04-18 23:20:29 -0400

    API

--- gitbranch.py
+++ gitbranch.py
@@ -9,9 +9,9 @@ from pygitutils import findbranch
 if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser()
+    p.add_argument('codepath',help='path to code root', nargs='?', default='~/code')
     p.add_argument('mainbranch',nargs='?',
                    default='master',help='name of your main branch')
-    p.add_argument('codepath',help='path to code root', nargs='?', default='~/code')
     p = p.parse_args()
 
     branch = findbranch(p.mainbranch, p.codepath)
