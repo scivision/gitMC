9a6df629e73eb60f6eab5ceb974567d1f5d82930
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-06-26 00:58:23 -0600

    modernize prereq spec

--- setup.py
+++ setup.py
@@ -1,31 +1,7 @@
 #!/usr/bin/env python
-install_requires=['colorama']
-tests_require=['pytest','nose','coveralls']
-# %%
-from setuptools import setup, find_packages
+from setuptools import setup
+from pathlib import Path
 
-setup(name='pygitutils',
-      packages=find_packages(),
-      description ='cross-platform git utilities for managing a large number of git repositories quickly',
-      long_description=open('README.rst').read(),
-      author = 'Michael Hirsch, Ph.D.',
-      version = '1.1.0',
-      url = 'https://github.com/scivision/pygitutils',
-      classifiers=[
-      'Development Status :: 5 - Production/Stable',
-      'Environment :: Console',
-      'Intended Audience :: Developers',
-      'Operating System :: OS Independent',
-      'Programming Language :: Python :: 3.6',
-      'Programming Language :: Python :: 3.7',
-      ],
-      install_requires=install_requires,
-      python_requires='>=3.6',
-      tests_require=tests_require,
-      extras_require={'tests':tests_require,
-                      'github':['pandas','numpy','tabulate','github']},
-      scripts=['ActOnChanged.py','find_missing_file.py','gitbranch.py',
-               'gitemail.py','git_filemode_windows.py','gtft.py',
-               'gtpl.py','gtps.py','ListAllGithubRepos.py',
-               ]
+setup(python_requires='>=3.6')
+      scripts = [s.name for s in Path(__file__).parent.glob('*.{sh,py}') if not s.name == 'setup.py']
 	  )
