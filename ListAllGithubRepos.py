0e89e481d81e8bc4cc47689f4c37ecbee7ebdf2e
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-08-09 13:00:30 -0400

    functionalize

--- ListAllGithubRepos.py
+++ ListAllGithubRepos.py
@@ -23,50 +23,58 @@ def listall(user: str, oauth: Path, branch: str=None, verbose: bool=False) -> Tu
 
     repos = list(g.get_user().get_repos())
     dat = pd.DataFrame(np.empty((len(repos), 2), int),
-                           index=[r.name for r in repos],
-                           columns=['forks', 'stars'])
+                       index=[r.name for r in repos],
+                       columns=['forks', 'stars'])
 
-    ahead = []
+    ahead: List[Tuple[str, int]] = []
 
     for repo in repos:
-        b = repo.default_branch if not branch else branch
-        try:
-            master = repo.get_branch(b)
-        except github.GithubException as e:
-            logging.error(f'{repo.full_name}  {e}')
-            continue
-
-        forks = repo.get_forks()
-        for fork in forks:
-            try:
-                fmaster = fork.get_branch(b)
-            except github.GithubException as e:
-                logging.error(f'{repo.full_name} {fork.full_name}  {e}')
-                continue
-
-            try:
-                comp = repo.compare(master.commit.sha, fmaster.commit.sha)
-            except github.GithubException as e:
-                if e.data['message'].startswith('No common ancestor'):
-                    continue
-
-                logging.error(f'{repo.full_name} {fork.full_name}  {e}')
-                continue
-
-            if comp.ahead_by:
-                ahead.append((fork.full_name, comp.ahead_by))
-                print(f'{fork.full_name} ahead by {comp.ahead_by}', end="")
-                if verbose and comp.behind_by:
-                    print(f'behind by {comp.behind_by}', end="")
-                print()
-
-            sleep(0.5)  # don't hammer the API, avoiding 502 errors
+        ahead = fork_prober(repo, ahead, branch)
 
         dat.loc[repo.name, :] = (repo.forks_count, repo.stargazers_count)
         sleep(0.1)  # don't hammer the API, avoiding 502 errors
 
-
     return dat, ahead
+
+
+def fork_prober(repo, ahead: List[Tuple[str, int]], branch: str=None,
+                verbose: bool=False) -> List[Tuple[str, int]]:
+
+    b = repo.default_branch if not branch else branch
+
+    try:
+        master = repo.get_branch(b)
+    except github.GithubException as e:
+        logging.error(f'{repo.full_name}  {e}')
+        return ahead
+
+    forks = repo.get_forks()
+    for fork in forks:
+        try:
+            fmaster = fork.get_branch(b)
+        except github.GithubException as e:
+            logging.error(f'{repo.full_name} {fork.full_name}  {e}')
+            continue
+
+        try:
+            comp = repo.compare(master.commit.sha, fmaster.commit.sha)
+        except github.GithubException as e:
+            if e.data['message'].startswith('No common ancestor'):
+                continue
+
+            logging.error(f'{repo.full_name} {fork.full_name}  {e}')
+            continue
+
+        if comp.ahead_by:
+            ahead.append((fork.full_name, comp.ahead_by))
+            print(f'{fork.full_name} ahead by {comp.ahead_by}', end="")
+            if verbose and comp.behind_by:
+                print(f'behind by {comp.behind_by}', end="")
+            print()
+
+        sleep(0.1)  # don't hammer the API, avoiding 502 errors
+
+    return ahead
 
 
 def main():
@@ -79,7 +87,7 @@ def main():
 
     datnz = dat[~(dat == 0).all(axis=1)]
 # %%
-    pd.set_option('display.max_rows',500)
+    pd.set_option('display.max_rows', 500)
 
     print(datnz.sort_values(['stars', 'forks'], ascending=False))
 
