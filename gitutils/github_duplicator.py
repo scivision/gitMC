dce964ab5f48bef1d83d4377a13acfdd114a26f0
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-09-23 12:54:43 -0400

    actually handle Wiki duplication

--- gitutils/github_duplicator.py
+++ gitutils/github_duplicator.py
@@ -3,10 +3,14 @@ from pathlib import Path
 import pandas
 import subprocess
 import tempfile
+from datetime import datetime
+import webbrowser
+from typing import Optional
 from .github_base import github_session, check_api_limit, repo_exists, last_commit_date
 
 
-def repo_dupe(repos: pandas.Series, oauth: Path, orgname: str = '', stem: str = ''):
+def repo_dupe(repos: pandas.Series, oauth: Path,
+              orgname: str = '', stem: str = ''):
     """
     fn: .xlsx file with repos to duplicate
     oauth: Path to your GitHub Oauth token  https://github.com/settings/tokens
@@ -47,29 +51,67 @@ def repo_dupe(repos: pandas.Series, oauth: Path, orgname: str = '', stem: str = 
             continue
 
         mirrorname = stem + email
-        newname = f'{username}/{mirrorname}'
-        newurl = f'ssh://github.com/{newname}'
 
+        gitdupe(oldurl, oldtime, username, mirrorname, op)
+        gitdupe(oldurl, None, username, mirrorname, op, iswiki=True)
+
+        sleep(0.1)
+
+
+def gitdupe(oldurl: str, oldtime: Optional[datetime],
+            username: str, mirrorname: str, op, iswiki: bool = False):
+
+    if iswiki:
+        oldurl += '.wiki.git'
+        mirrorname += '.wiki.git'
+
+    newname = f'{username}/{mirrorname}'
+    newurl = f'ssh://github.com/{newname}'
+
+    if not iswiki:
         exists = repo_exists(op, mirrorname)
         if exists:
             newrepo = op.get_repo(mirrorname)
             if newrepo.pushed_at >= oldtime:
-                continue
+                return
 
-        print('\n', email, oldurl, '\n')
-        with tempfile.TemporaryDirectory() as d:
-            tmprepo = Path(d)
-            # 1. bare clone
-            subprocess.check_call(['git', 'clone', '--bare', oldurl],
-                                  stdout=subprocess.DEVNULL, cwd=tmprepo)
+        print('\n', oldurl, '\n')
+    else:
+        try:
+            subprocess.check_call(['git', 'ls-remote', '--exit-code', newurl])
+            return
+        except subprocess.CalledProcessError:
+            exists = True
 
-            # 2. create new repo
-            if not exists:
-                op.create_repo(name=mirrorname, private=True)
+    with tempfile.TemporaryDirectory() as d:
+        tmprepo = Path(d)
+        # 1. bare clone
+        cmd = ['git', 'clone', oldurl] if iswiki else ['git', 'clone', '--bare', oldurl]
+        subprocess.check_call(cmd, stdout=subprocess.DEVNULL, cwd=tmprepo)
 
-            # 3. mirror to new repo
-            pwd = (tmprepo / (oldurl.split('/')[-1])).with_suffix('.git')
+        # 2. create new repo
+        if not exists:
+            op.create_repo(name=mirrorname, private=True, has_wiki=True)
+
+        # 3. mirror to new repo
+
+        if iswiki:
+            pwd = (tmprepo / (oldurl.split('/')[-1]).split('.git')[0])
+
+            subprocess.check_call(['rm', '-rf', '.git'], cwd=pwd)
+            subprocess.check_call(['git', 'init'], cwd=pwd)
+            subprocess.check_call(['git', 'remote', 'add', 'origin', newurl], cwd=pwd)
+            subprocess.check_call(['git', 'add', '.'], cwd=pwd)
+            subprocess.check_call(['git', 'commit', '-am', 'duplicate'], cwd=pwd)
+
+            browseurl = newurl
+            browseurl = browseurl.replace('ssh', 'https').replace('.wiki.git', '/wiki')
+            webbrowser.open_new_tab(browseurl)
+
+            subprocess.check_call(['git', 'push', '-f', '-u', 'origin', 'master'], cwd=pwd)
+        else:
+            pwd = (tmprepo / (oldurl.split('/')[-1]))
+            pwd = pwd.with_suffix('.git')
+
             cmd = ['git', 'push', '--mirror', newurl]
             subprocess.check_call(cmd, cwd=pwd)
-
-        sleep(0.1)
