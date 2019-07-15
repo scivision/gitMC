728c388a428f63a43e83957e04d70d31575b0b31
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-07-15 14:41:10 -0400

    allow multiple username / organization names

--- CountGithubStars.py
+++ CountGithubStars.py
@@ -3,30 +3,33 @@
 Totals up stars and fork count for all repos of a Github user.
 Can use Oauth login if desired.
 """
-import pandas as pd
+import pandas
 from argparse import ArgumentParser
 import gitutils.github_repo_stats as gu
-import pandas
 
 
 def main():
     p = ArgumentParser(description='Totals up stars and fork count for all repos of a Github user.')
-    p.add_argument('user', help='Github username')
-    p.add_argument('oauth', help='Oauth filename', nargs='?')
+    p.add_argument('user', help='Github username / organization name', nargs='+')
+    p.add_argument('-i', '--oauth', help='Oauth filename', nargs='?')
     p = p.parse_args()
 
-    counts = gu.repo_prober(p.user, p.oauth, None, True)[0]
+    dat = pandas.DataFrame(None)
 
-    dat = pd.DataFrame([c[1:] for c in counts],
-                       index=[c[0] for c in counts],
-                       columns=['forks', 'stars'])
+    for u in p.user:
+        counts = gu.repo_prober(u, p.oauth, None, True)[0]
+
+        d = pandas.DataFrame([c[1:] for c in counts],
+                             index=[c[0] for c in counts],
+                             columns=['forks', 'stars'])
+        dat = pandas.concat((dat, d))
 
     datnz = dat[~(dat == 0).all(axis=1)]
 # %%  Stars and Forks
-    pd.set_option('display.max_rows', 500)
+    pandas.set_option('display.max_rows', 500)
 
-    print(f'\n{p.user} total stars received {dat["stars"].sum()}')
-    print(f'{p.user} total other users forked {dat["forks"].sum()}\n')
+    print(f'\n{" ".join(p.user)} total stars received {dat["stars"].sum()}')
+    print(f'{" ".join(p.user)} total other users forked {dat["forks"].sum()}\n')
 
     print(datnz.sort_values(['stars', 'forks'], ascending=False))
 
