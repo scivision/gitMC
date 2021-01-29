192276327e3d0247a9d65f1ab0d275e94135430d
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2021-01-29 14:40:44 -0500

    gitbranch -switch old new to switch branchs from old to new

--- src/gitutils/branch.py
+++ src/gitutils/branch.py
@@ -10,6 +10,7 @@ import typing as T
 from pathlib import Path
 import asyncio
 import logging
+import subprocess
 
 from . import _log
 from .git import GITEXE, gitdirs
@@ -18,6 +19,7 @@ BRANCH_REV = ["rev-parse", "--abbrev-ref", "HEAD"]
 BRANCH_SYM = ["symbolic-ref", "--short", "HEAD"]
 BRANCH_NAME = ["name-rev", "--name-only", "HEAD"]
 BRANCH_SIMPLE = ["branch", "--show-current"]  # Git >= 2.22
+SWITCH = ["switch"]
 
 
 async def different_branch(main: T.Sequence[str], path: Path) -> T.Tuple[str, str]:
@@ -68,20 +70,47 @@ async def git_branch(branch: T.Sequence[str], path: Path) -> T.List[T.Tuple[str,
     return different
 
 
+def branch_switch(path: Path, old_branch: str, new_branch: str):
+
+    for d in gitdirs(path):
+        cmd = [GITEXE, "-C", str(d)] + BRANCH_SIMPLE
+        current = subprocess.check_output(cmd, text=True, timeout=5).strip()
+        if current == new_branch:
+            continue
+        if current != old_branch:
+            logging.info(f"not changing: {d.name}: {current} != {old_branch}")
+            continue
+
+        cmd = [GITEXE, "-C", str(d)] + BRANCH_SIMPLE
+        ret = subprocess.run([GITEXE, "-C", str(d)] + SWITCH + [new_branch], stderr=subprocess.PIPE, timeout=10, text=True)
+        if ret.returncode != 0:
+            stderr = ret.stderr.strip()
+            if stderr == f"fatal: invalid reference: {new_branch}":
+                continue
+            else:
+                raise ValueError(f"{d} could not switch to {new_branch} from {old_branch}: {stderr}")
+
+        print(d.name, old_branch, "=>", new_branch)
+
+
 def cli():
     """
     report on git repos not on the expected branch e.g. 'master'
     """
 
-    p = argparse.ArgumentParser()
+    p = argparse.ArgumentParser(description="check for non-default branches, and mass switch branches")
     p.add_argument("path", help="path to look under", nargs="?", default="~/code")
     p.add_argument("-v", "--verbose", action="store_true")
     p.add_argument("-main", nargs="+", default=["main", "master"], help="name of your main branch")
+    p.add_argument("-switch", nargs=2, help="switch branch from OLD to NEW, if NEW is available")
     P = p.parse_args()
 
     _log(P.verbose)
 
-    asyncio.run(git_branch(P.main, P.path))
+    if P.switch is None:
+        asyncio.run(git_branch(P.main, P.path))
+    else:
+        branch_switch(P.path, *P.switch)
 
 
 if __name__ == "__main__":
